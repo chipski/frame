a24b2d121bfe3da36a651293ca162223d558b4fb
Chip Vanek <chipski@mac.com>
2013-12-23 12:17:24 -0800

    reworking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,54 +7,49 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :except => [:private] 
   
   def index
-    
   end
-  
   def start
     render :index
   end
-  
   def another
     render :index
   end
-
   def paper
     render :index
   end
-
   def next
     render :index
   end
-
   def signup
     render :index
   end
-  
+
   def more
   end
 
   def private
     render :index
   end
-  
+
   def critter
     #render :critter, :layout=>false
-    render :partial=>"home/critter"
+    render :partial=>"home/frame"
   end
-
   def critter2
     #render :critter, :layout=>false
     render :partial=>"home/frame3"
   end
+  def critter_iframe
+    render :partial=>"home/critter_iframe"
+  end
+
   
   def pardot
     render :partial=>"home/frame2", :layout=>false
   end
-  
   def pardot_flat
     render :partial=>"home/frame2"
   end  
-
   def pardot_iframe
     render :partial=>"home/pardot_iframe"
   end
